5f396daba4fc184ceb0eba77241befe69b7d71e2
Ankur Rajbongshi <dekalasit@gmail.com>
2025-10-12 18:41:05 +0530

    modified .env for netlify

--- src/utils/lib/dynamoClient.ts
+++ src/utils/lib/dynamoClient.ts
@@ -4,8 +4,8 @@ import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 const client = new DynamoDBClient({
     region: 'ap-south-1',
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_ID!
+        accessKeyId: process.env.AWS_ACCESS_ID!,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_ID!
     }
 })
 
