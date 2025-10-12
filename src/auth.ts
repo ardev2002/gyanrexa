0e312568c14d43f2ce6511a469d37ed881ebb778
Ankur Rajbongshi <dekalasit@gmail.com>
2025-10-12 20:08:37 +0530

    .env modified-2

--- src/auth.ts
+++ src/auth.ts
@@ -3,8 +3,8 @@ import Google from "next-auth/providers/google"
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [
         Google({
-            clientId: process.env.AUTH_GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.AUTH_GOOGLE_CLIENT_SECRET!
+            clientId: process.env.AUTH_WEBAPP_GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.AUTH_WEBAPP_GOOGLE_CLIENT_SECRET!
         })
     ],
 })
